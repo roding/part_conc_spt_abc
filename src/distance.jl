0bf5a16a1ffde230a34d602b021987b64007a6be
Magnus Röding <magnus.roding@gmail.com>
2017-01-03 13:35:53 +0100

    First version of distance test in Matlab

--- src/distance.jl
+++ src/distance.jl
@@ -1,2 +1,3 @@
 function distance(K1::Array{Int64, 1}, RSQ1::Array{Float64, 1}, K2::Array{Int64, 1}, RSQ2::Array{Float64, 1})
-	number_of_trajectories_1::Int64 
+	n1::Int64 = length(K1)
+	n2::Int64 = length(K2)
