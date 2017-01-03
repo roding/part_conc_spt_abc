ae44de29af2f61557634359df7f83be22edce76e
Magnus Röding <magnus.roding@gmail.com>
2017-01-03 11:51:20 +0100

    Randomly initializing particle position

--- src/simulate_system.jl
+++ src/simulate_system.jl
@@ -1,4 +1,4 @@
-function simulate_system(mu::Float64, sigma::Float64, c::Float64, ax::Float64, ay::Float64, az::Float64, A::Float64, number_of_frames::Array{Int64, 1})
+function simulate_system(mu::Float64, sigma::Float64, c::Float64, ax::Float64, ay::Float64, az::Float64, L::Float64, number_of_frames::Array{Int64, 1})
 	
 	#kmax::Int64 = maximum(nFrames)
 	#H::Array{Int64,1} = zeros(kmax)
@@ -6,32 +6,46 @@ function simulate_system(mu::Float64, sigma::Float64, c::Float64, ax::Float64, a
 	# Convert concentration to a number of particles. The factor 1e12 takes into account
 	# that concentration is specified in particles/ml. Also enlarge simulation domain
 	# to accomodate an integer number of particle while maintaining correct concentration.
-	number_of_particles_temp::Float64 = convert(Int64, c * A^3 / 1e12)
+	number_of_particles_temp::Float64 = convert(Int64, c * L^3 / 1e12)
 	number_of_particles::Int64 = convert(Int64, ceil(number_of_particles_temp))
-	A = (convert(Float64, number_of_particles) / number_of_particles_temp)^(1.0/3.0) * A
+	L = (convert(Float64, number_of_particles) / number_of_particles_temp)^(1.0/3.0) * L
 	
 	# Define lower and upper bounds for the detection region.
-	lbx::Float64 = 0.5 * (A - ax)
-	ubx::Float64 = 0.5 * (A + ax)
-	lby::Float64 = 0.5 * (A - ay)
-	uby::Float64 = 0.5 * (A + ay)
-	lbz::Float64 = 0.5 * (A - az)
-	ubz::Float64 = 0.5 * (A + az)
+	lbx::Float64 = 0.5 * (L - ax)
+	ubx::Float64 = 0.5 * (L + ax)
+	lby::Float64 = 0.5 * (L - ay)
+	uby::Float64 = 0.5 * (L + ay)
+	lbz::Float64 = 0.5 * (L - az)
+	ubz::Float64 = 0.5 * (L + az)
 	
+	# Simulate.
+	count::Int64 = 0
+	x::Float64 = 0.0
+	y::Float64 = 0.0
+	z::Float64 = 0.0
+	D::Float64 = 0.0
+	s::Float64 = 0.0
+	number_of_videos::Int64 = length(number_of_frames)
 	
-		count = 0
-	for currentVideo = 1:nVideos
-		for currentParticle = 1:nParticles
-			x = A_corr * rand()
-			y = A_corr * rand()
-			z = A_corr * rand()
-			if (zlo <= z) & (z <= zhi) & (xlo <= x) & (x <= xhi) & (ylo <= y) & (y <= yhi)
+	for current_video = 1:number_of_videos
+		for current_particle = 1:number_of_particles
+			# Random initial position.
+			x = L * rand()
+			y = L * rand()
+			z = L * rand()
+			
+			# In detection region or not?
+			if (lbz <= z) & (z <= ubz) & (lbx <= x) & (x <= ubx) & (lby <= y) & (y <= uby)
 				count = 1
 			else
 				count = 0
 			end
-			for currentFrame = 2:nFrames[currentVideo]
-				x += sigma*randn()
+			
+			# Generate random diffusion coefficent from distribution.
+			
+			
+			for current_frame = 2:number_of_frames[current_video]
+				x = x + sigma*randn()
 				y += sigma*randn()
 				z += sigma*randn()
 				if x > A_corr
@@ -49,13 +63,19 @@ function simulate_system(mu::Float64, sigma::Float64, c::Float64, ax::Float64, a
 				elseif z < 0.0
 					z += A_corr
 				end
-				if (zlo <= z) & (z <= zhi) & (xlo <= x) & (x <= xhi) & (ylo <= y) & (y <= yhi)
+				if (lbz <= z) & (z <= ubz) & (lbx <= x) & (x <= ubx) & (lby <= y) & (y <= uby)
 					count += 1
 				elseif count > 0
 					Hsim[count] += 1
 					count = 0
 				end
 			end
+		
+	for currentVideo = 1:nVideos
+		for currentParticle = 1:nParticles
+			
+			
+			
 			if count > 0
 				Hsim[count] += 1
 			end
