e4d2695c18e5e788f9ed0b41377f4ccc70648e96
Magnus Röding <magnus.roding@gmail.com>
2017-01-03 17:05:08 +0100

    Completer Poisson random number generation

--- src/rand_poisson.jl
+++ src/rand_poisson.jl
@@ -1,8 +1,14 @@
+# Algorithm for generating Poisson distributed random numbers.
+# Attributed to Donald Knuth, see Wikipedia.
+
 function rand_poisson(lambda)
 	x::Int64 = 0
-	
-	
-	
-	
-	return x
+	L::Float64 = exp(-lambda)
+	p::Float64 = 1
+    
+	while p > L
+         x = x + 1
+         p = p * rand()
+    end
+    return x - 1
 end
