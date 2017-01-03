813b47686017f1c04237047b915a3961a3b5a879
Magnus Röding <magnus.roding@gmail.com>
2017-01-03 19:15:11 +0100

    Implemented new distance metric

--- src/distance2.jl
+++ src/distance2.jl
@@ -3,25 +3,22 @@ function distance2(K1::Array{Int64, 1}, RSQ1::Array{Float64, 1}, K2::Array{Int64
 	n2::Int64 = length(K2)
 	
 	d::Float64 = 0.0
-	p1::Float64 = 0.0
-	p2::Float64 = 0.0
 	
-	# Evaluate 2-D ECDF for both data sets, at points in data set 1.
-	for current_point_1 = 1:n1
-		p1 = sum( (K1 .<= K1[current_point_1]) & (RSQ1 .<= RSQ1[current_point_1]) )
-		p2 = sum( (K2 .<= K1[current_point_1]) & (RSQ2 .<= RSQ1[current_point_1]) )
-		
-		d = d + (p1 - p2)^2
+	sort!(K1)
+	sort!(K2)
+	sort!(RSQ1)
+	sort!(RSQ2)
+	
+	if n1 <= n2
+		d = d + sum((K1 - K2[1:n1]).^2) + sum(K2[n1+1:end].^2)
+	else
+		d = d + sum((K1[1:n2] - K2).^2) + sum(K1[n2+1:end].^2)
 	end
-
-	# Evaluate 2-D ECDF for both data sets, at points in data set 2.
-	#for current_point_2 = 1:n2
-	#	p1 = sum( (K1 .<= K2[current_point_2]) & (RSQ1 .<= RSQ2[current_point_2]) )
-	#	p2 = sum( (K2 .<= K2[current_point_2]) & (RSQ2 .<= RSQ2[current_point_2]) )
-	#	
-	#	d = d + (p1 - p2)^2
-	#end
+	if n1 <= n2
+		d = d + sum((RSQ1 - RSQ2[1:n1]).^2) + sum(RSQ2[n1+1:end].^2)
+	else
+		d = d + sum((RSQ1[1:n2] - RSQ2).^2) + sum(RSQ1[n2+1:end].^2)
+	end
 	
-	d = d / convert(Float64, n1*n2)
 	return d
 end
