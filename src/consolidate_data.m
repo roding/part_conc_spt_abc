448e44697ac736db3a45fe4e43954c9347d409ae
Magnus Röding <magnus.roding@gmail.com>
2017-01-10 13:46:01 +0100

    Data consolidtion done

--- src/consolidate_data.m
+++ src/consolidate_data.m
@@ -2,27 +2,53 @@ clear
 clc
 close all hidden
 
-%% Read data.
-file_name = 'abc_sample.dat';
-file_info = dir(file_name);
-file_size = file_info.bytes;
 number_of_columns = 5;
 precision_in_bytes = 8;
-number_of_abc_samples = file_size / number_of_columns / precision_in_bytes;
+    
+folder = 'res_20170109';
+files = dir([folder '/' '*.dat']);
+number_of_files = numel(files);
 
-file_id = fopen(file_name);
-data = fread(file_id, [number_of_columns, number_of_abc_samples], 'float64');
-fclose(file_id);
+number_of_files = 100;
 
-data = data';
+mu = [];
+sigma = [];
+c = [];
+az = [];
+dist = [];
+    
+for current_file = 1:number_of_files
+    disp(current_file)
+    file_path =  [folder '/' files(current_file).name];
+    file_info = dir(file_path);
+    file_size = file_info.bytes;
 
-mu = data(:, 1);
-sigma = data(:, 2);
-c = data(:, 3);
-az = data(:, 4);
-dist  = data(:, 5);
+    number_of_abc_samples = file_size / number_of_columns / precision_in_bytes;
 
-clear data
+    file_id = fopen(file_path);
+    data = fread(file_id, [number_of_columns, number_of_abc_samples], 'float64');
+    fclose(file_id);
+
+    data = data';
+
+%     mu{current_file} = data(:, 1);
+%     sigma{current_file} = data(:, 2);
+%     c{current_file} = data(:, 3);
+%     az{current_file} = data(:, 4);
+%     dist{current_file}  = data(:, 5);
+    
+%     mu = [mu ; data(:, 1)];
+%     sigma =  [sigma ; data(:, 2)];
+%     c = [c ; data(:, 3)];
+%     az = [az ; data(:, 4)];
+%     dist = [dist ; data(:, 5)];
+    
+    mu = cat(1, mu, data(:, 1));
+    sigma = cat(1, sigma, data(:, 2)); 
+    c = cat(1, c, data(:, 3));
+    az = cat(1, az, data(:, 4));
+    dist = cat(1, dist, data(:, 5));
+end
 
 %% Try inference.
 p = 0.00005;
@@ -34,7 +60,7 @@ sigma = sigma(index);
 c = c(index);
 az = az(index);
 
-figure, hold on, hist(mu), title('mu'), hold off
-figure, hold on, hist(sigma), title('sigma'), hold off
-figure, hold on, hist(c), title('c'), hold off
-figure, hold on, hist(az), title('az'), hold off
+figure, hold on, hist(mu, 100), title('mu'), hold off
+figure, hold on, hist(sigma, 100), title('sigma'), hold off
+figure, hold on, hist(c, 100), title('c'), hold off
+figure, hold on, hist(az, 100), title('az'), hold off
