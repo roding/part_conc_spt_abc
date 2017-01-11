765248ee093b7ab40e3955db415d1317bbcb1b0e
Magnus Röding <magnus.roding@gmail.com>
2017-01-11 15:19:26 +0100

    Changed notation

--- src/distance2.jl
+++ src/distance2.jl
@@ -1,4 +1,4 @@
-function distance2(K1::Array{Int64, 1}, RSQ1::Array{Float64, 1}, K2::Array{Int64, 1}, RSQ2::Array{Float64, 1})
+function distance2(K1::Array{Int64, 1}, DE1::Array{Float64, 1}, K2::Array{Int64, 1}, DE2::Array{Float64, 1})
 	n1::Int64 = length(K1)
 	n2::Int64 = length(K2)
 	
@@ -6,8 +6,8 @@ function distance2(K1::Array{Int64, 1}, RSQ1::Array{Float64, 1}, K2::Array{Int64
 	
 	sort!(K1)
 	sort!(K2)
-	sort!(RSQ1)
-	sort!(RSQ2)
+	sort!(DE1)
+	sort!(DE2)
 	
 	if n1 <= n2
 		d = d + sum((K1 - K2[1:n1]).^2) + sum(K2[n1+1:end].^2)
@@ -15,9 +15,9 @@ function distance2(K1::Array{Int64, 1}, RSQ1::Array{Float64, 1}, K2::Array{Int64
 		d = d + sum((K1[1:n2] - K2).^2) + sum(K1[n2+1:end].^2)
 	end
 	if n1 <= n2
-		d = d + sum((RSQ1 - RSQ2[1:n1]).^2) + sum(RSQ2[n1+1:end].^2)
+		d = d + sum((DE1 - DE2[1:n1]).^2) + sum(DE2[n1+1:end].^2)
 	else
-		d = d + sum((RSQ1[1:n2] - RSQ2).^2) + sum(RSQ1[n2+1:end].^2)
+		d = d + sum((DE1[1:n2] - DE2).^2) + sum(DE1[n2+1:end].^2)
 	end
 	
 	return d
