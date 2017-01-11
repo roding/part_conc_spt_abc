ab2d4b2ee2a216e9a15b538f57cf2f8ed80f0288
Magnus Röding <magnus.roding@gmail.com>
2017-01-11 18:06:10 +0100

    Rewrote module function in modify in argument form

---
+++ src/periodic!.jl
@@ -0,0 +1,12 @@
+# Simple modulo function for imposing periodic boundary conditions.
+
+function periodic!(x::Float64, L::Float64)
+	if x > L
+		x = x - L
+	elseif x < 0.0
+		x = x + L
+	end
+	
+	nothing
+end
+
