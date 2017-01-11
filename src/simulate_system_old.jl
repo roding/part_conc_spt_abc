6343aa1a9fb6a4dcbc0d7fab5f069daaea3d35c9
Magnus Röding <magnus.roding@gmail.com>
2017-01-11 17:47:56 +0100

    Switched to a combined system simulation routine for all distributions

--- src/simulate_system_old.jl
+++ src/simulate_system_old.jl
@@ -1,15 +1,14 @@
 include("rand_poisson.jl")
 
-function simulate_system_monodisperse(D::Float64, c::Float64, ax::Float64, ay::Float64, az::Float64, L::Float64, number_of_frames::Array{Int64, 1}, deltat::Float64, kmin::Int64)
-	
-	# THIS COMMENT IS INCORRECT!!!!!!!!!
-	# Convert concentration to a number of particles. The factor 1e12 takes into ack
-	# that concentration is specified in particles/ml. Also enlarge simulation domain
-	# to accomodate an integer number of particle while maintaining correct concentration.
+function simulate_system(mu::Float64, sigma::Float64, c::Float64, ax::Float64, ay::Float64, az::Float64, L::Float64, number_of_frames::Array{Int64, 1}, deltat::Float64, kmin::Int64)
+	# Intensity of Poisson distribution of the number of particles. The factor 
+	# 1e12 takes into account that concentration is specified in particles/ml.
 	lambda::Float64 = c * L^3 / 1e12
+	
+	# Number of particles in simulated system, which varies between videos.
 	number_of_particles::Int64 = 0
-
-	# Define lower and upper bounds for the detection region.
+	
+	# Lower and upper bounds for the detection region.
 	lbx::Float64 = 0.5 * (L - ax)
 	ubx::Float64 = 0.5 * (L + ax)
 	lby::Float64 = 0.5 * (L - ay)
@@ -19,31 +18,29 @@ function simulate_system_monodisperse(D::Float64, c::Float64, ax::Float64, ay::F
 	
 	# Simulate.
 	k::Int64 = 0
-	de::Float64 = 0.0
+	rsq::Float64 = 0.0
 	x::Float64 = 0.0
 	y::Float64 = 0.0
 	z::Float64 = 0.0
 	deltax::Float64 = 0.0
 	deltay::Float64 = 0.0
 	deltaz::Float64 = 0.0
+	D::Float64 = 0.0
+	s::Float64 = 0.0
 	number_of_videos::Int64 = length(number_of_frames)
 	
-	s::Float64 = sqrt(2 * D * deltat) # Standard deviation of random displacements.
-	
 	K::Array{Int64, 1} = zeros(0)
-	DE::Array{Float64, 1} = zeros(0)
+	RSQ::Array{Float64, 1} = zeros(0)
 	
 	for current_video = 1:number_of_videos
-		number_of_particles = rand_poisson(lambda)
-		
 		for current_particle = 1:number_of_particles
 			# Random initial position.
 			x = L * rand()
 			y = L * rand()
 			z = L * rand()
 			
-			# Reset empirical diffusion coefficient.
-			de = 0.0
+			# Reset rsq.
+			rsq = 0.0
 			
 			# In detection region or not? Check z limits first because they are more restrictive.
 			if (lbz <= z <= ubz) & (lbx <= x <= ubx) & (lby <= y <= uby)
@@ -52,11 +49,15 @@ function simulate_system_monodisperse(D::Float64, c::Float64, ax::Float64, ay::F
 				k = 0
 			end
 			
+			# Generate random diffusion coefficent from distribution.
+			D = exp(mu + sigma * rand())
+			s = sqrt(2 * D * deltat)
+			
 			# Let particle diffuse through remainder of video.
 			for current_frame = 2:number_of_frames[current_video]
-				deltax = s * randn()
-				deltay = s * randn()
-				deltaz = s * randn()
+				deltax  = s * randn()
+				deltay  = s * randn()
+				deltaz  = s * randn()
 				x = x + deltax
 				y = y + deltay
 				z = z + deltaz
@@ -81,26 +82,19 @@ function simulate_system_monodisperse(D::Float64, c::Float64, ax::Float64, ay::F
 				
 				if (lbz <= z <= ubz) & (lbx <= x <= ubx) & (lby <= y <= uby)
 					k = k + 1
-					if k >= 2 # The first point inside the detection region does not contributed toward the estimated diffusion coefficient.
-						de = de + deltax^2 + deltay^2 # Only in x-y plane.
-					end
+					rsq = rsq + deltax^2 + deltay^2 # Only in x-y plane.
 				elseif k > 0
 					if k >= kmin
 						push!(K, k)
-						if k >= 2
-							de = de / (convert(Float64, k - 1) * 4.0 * deltat) # The '4' comes from the 2-D obs.
-						else
-							de = 0.0
-						end
-						push!(DE, de)
+						push!(RSQ, rsq)
 					end
 					k = 0
-					de = 0.0
+					rsq = 0.0
 				end
 			end
 		end
 	end
 	
-	return (K, DE)
+	return (K, RSQ)
 end
 
