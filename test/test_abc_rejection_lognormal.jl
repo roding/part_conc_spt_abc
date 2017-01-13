d89c8100814ad8f2cf39f942090a75ad97847038
Magnus Röding <magnus.roding@gmail.com>
2017-01-13 15:08:06 +0100

    Changed parametrization of lognormal case in test routine to mean/std instead of mu/sigma

--- test/test_abc_rejection_lognormal.jl
+++ test/test_abc_rejection_lognormal.jl
@@ -22,34 +22,31 @@ function test_abc_rejection_lognormal()
 	distribution_class::String = "lognormal"
 	m_real::Float64 = 2.5 # µm^2/s.
 	s_real::Float64 = 0.5 # µm^2/s.
-	mu_real::Float64 = log(m_real) - 0.5 * log(1 + s_real^2/m_real^2)
-	sigma_real::Float64 = sqrt(log(1 + s_real^2/m_real^2))
+	#mu_real::Float64 = log(m_real) - 0.5 * log(1 + s_real^2/m_real^2)
+	#sigma_real::Float64 = sqrt(log(1 + s_real^2/m_real^2))
 	#println((mu_real, sigma_real))
-	distribution_parameters_real::Array{Float64, 1} = [mu_real, sigma_real]
 	c_real::Float64 = 1e10 # part/ml.
 	az_real::Float64 = 2.0 # µm.
 	
 	# Simulate system.
-	(K_real, DE_real) = simulate_system(distribution_class, distribution_parameters_real, c_real, ax, ay, az_real, Lx, Ly, Lz, number_of_frames, deltat, kmin)
+	(K_real, DE_real) = simulate_system(distribution_class, [m_real, s_real], c_real, ax, ay, az_real, Lx, Ly, Lz, number_of_frames, deltat, kmin)
 
 	# Parameter bounds for inference.
-	lb_mu::Float64 = 0.25 * mu_real
-	ub_mu::Float64 = 4.0 * mu_real
-	lb_sigma::Float64 = 0.25 * sigma_real
-	ub_sigma::Float64 = 4.0 * sigma_real
+	lb_m::Float64 = 0.25 * m_real
+	ub_m::Float64 = 4.0 * m_real
+	lb_s::Float64 = 0.25 * s_real
+	ub_s::Float64 = 4.0 * s_real
 	lb_c::Float64 = 0.25 * c_real
 	ub_c::Float64 = 4.0 * c_real
 	lb_az::Float64 = 0.25 * az_real
 	ub_az::Float64 = 4.0 * az_real
-#	lb::Array{Float64, 1} = [0.5 * mu_real, 0.5 * sigma_real, 0.5 * c_real, 0.5 * az_real]
-#	ub::Array{Float64, 1} = [2.0 * mu_real, 2.0 * sigma_real, 2.0 * c_real, 2.0 * az_real]
 			
 	# Inference parameters.
-	number_of_abc_samples::Int64 = 100000
+	number_of_abc_samples::Int64 = 100
 
 	# Variables for candidate parameter values.
-	mu_sim::Float64 = 0.0
-	sigma_sim::Float64 = 0.0
+	m_sim::Float64 = 0.0
+	s_sim::Float64 = 0.0
 	c_sim::Float64 = 0.0
 	az_sim::Float64 = 0.0
 	
@@ -65,16 +62,16 @@ function test_abc_rejection_lognormal()
 			println(current_abc_sample)
 		end
 		
-		mu_sim = lb_mu + (ub_mu - lb_mu) * rand()
-		sigma_sim = lb_sigma + (ub_sigma - lb_sigma) * rand()
+		m_sim = lb_m + (ub_m - lb_m) * rand()
+		s_sim = lb_s + (ub_s - lb_s) * rand()
 		c_sim = lb_c + (ub_c - lb_c) * rand()
 		az_sim = lb_az + (ub_az - lb_az) * rand()
 		
-		(K_sim, DE_sim) = simulate_system(distribution_class, [mu_sim, sigma_sim], c_sim, ax, ay, az_sim, Lx, Ly, Lz, number_of_frames, deltat, kmin)
+		(K_sim, DE_sim) = simulate_system(distribution_class, [m_sim, s_sim], c_sim, ax, ay, az_sim, Lx, Ly, Lz, number_of_frames, deltat, kmin)
 
 		dist = distance(K_real, DE_real, K_sim, DE_sim)
 		#@time 
-		write(file_stream_output, mu_sim, sigma_sim, c_sim, az_sim, dist)
+		write(file_stream_output, m_sim, s_sim, c_sim, az_sim, dist)
 	end
 	close(file_stream_output)
 	
