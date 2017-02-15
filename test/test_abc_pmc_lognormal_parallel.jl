24b6f8632f35d55522c9b130783f9938efe6a91f
Magnus Röding <magnus.roding@gmail.com>
2017-02-15 08:44:22 +0100

    Merge

--- test/test_abc_pmc_lognormal_parallel.jl
+++ test/test_abc_pmc_lognormal_parallel.jl
@@ -16,9 +16,9 @@ function test_abc_pmc_lognormal_parallel()
 	# Acquisition parameters.
 	ax::Float64 = 40.0 # µm.
 	ay::Float64 = 40.0 # µm.
-	Lx::Float64 = 100.0 # µm.
-	Ly::Float64 = 100.0 # µm.
-	Lz::Float64 = 50.0 # µm.
+	Lx::Float64 = 60.0#100.0 # µm.
+	Ly::Float64 = 60.0#100.0 # µm.
+	Lz::Float64 = 10.0#50.0 # µm.
 	number_of_frames::Array{Int64, 1} = 250 * ones(40)
 	deltat::Float64 = 0.05 # seconds
 	kmin::Int64 = 2
@@ -28,7 +28,7 @@ function test_abc_pmc_lognormal_parallel()
 	distribution_class::String = "lognormal"
 	m_real::Float64 = 2.5 # µm^2/s.
 	s_real::Float64 = 0.5 # µm^2/s.
-	c_real::Float64 = 5e9 # part/ml.
+	c_real::Float64 = 1e10 # part/ml.
 	az_real::Float64 = 2.0 # µm.
 	
 	# Simulate system.
@@ -37,7 +37,7 @@ function test_abc_pmc_lognormal_parallel()
 	
 	# Distance function parameters.
 	k_bin_edges::Array{Float64, 1} = 0.5:1:kmax+0.5
-	de_bin_edges::Array{Float64, 1} = 0.0:0.1:12.5
+	de_bin_edges::Array{Float64, 1} = 0.0:0.01:12.5 #0.0:0.1:12.5
 	
 	n_K_real::Array{Int64, 1} = zeros(length(k_bin_edges) - 1)
 	n_DE_real::Array{Int64, 1} = zeros(length(de_bin_edges) - 1)
@@ -54,7 +54,7 @@ function test_abc_pmc_lognormal_parallel()
 	# Parameter bounds for inference.
 	lb_m::Float64 = 0.25 * m_real
 	ub_m::Float64 = 4.0 * m_real
-	lb_s::Float64 = 0.25 * s_real
+	lb_s::Float64 = 0.0#0.25 * s_real
 	ub_s::Float64 = 4.0 * s_real
 	lb_c::Float64 = 0.25 * c_real
 	ub_c::Float64 = 4.0 * c_real
@@ -62,8 +62,8 @@ function test_abc_pmc_lognormal_parallel()
 	ub_az::Float64 = 4.0 * az_real#az_real
 		
 	# Inference parameters.
-	number_of_abc_samples::Int64 = 1#28
-	number_of_iterations::Int64 = 1#5000
+	number_of_abc_samples::Int64 = 512
+	number_of_iterations::Int64 = 5000
 
 	# Variables for population parameter values.
 	m::Array{Float64, 1} = zeros(number_of_abc_samples)
@@ -93,7 +93,7 @@ function test_abc_pmc_lognormal_parallel()
 	tau_az::Float64 = sqrt( 2.0 * var(az, corrected = false) )
 	
 	# The rest of the iterations.
-	gamma = 8.0
+	gamma = 8.5
 	delta_gamma = 0.005
 	epsilon::Float64 = 10^gamma
 	trial_count::SharedArray{Int64, 1} = [0]
@@ -164,11 +164,10 @@ function test_abc_pmc_lognormal_parallel()
 				(~, n_K_sim) = hist(K_sim, k_bin_edges)
 				(~, n_DE_sim) = hist(DE_sim, de_bin_edges)
 				dist_bis = distance(n_K_real, n_DE_real, n_K_sim, n_DE_sim)
-				println(dist_bis)
+				#println(dist_bis)
 				
 				trial_count[1] = trial_count[1] + 1
 			end
-			
 			
 			m_star[current_abc_sample] = m_bis
 			s_star[current_abc_sample] = s_bis
