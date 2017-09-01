89045d95a541f0333135915c27739cd546e06d0c
Magnus Röding <magnus.roding@gmail.com>
2017-09-01 08:40:16 +0200

    More effective distance computation

--- src/cpu/distance.jl
+++ src/cpu/distance.jl
@@ -1,8 +1,8 @@
 function distance(H::Array{Int64, 2}, H_sim::Array{Int64, 2})
 	
-	cum_H::Array{Float64, 2} = cumsum(cumsum(H, 1), 2)
-	cum_H_sim::Array{Float64, 2} = cumsum(cumsum(H_sim, 1), 2)
-	d::Float64 = sum( (cum_H - cum_H_sim).^2 )
+	dH::Array{Float64, 2} = H - H_sim
+	cum_dH::Array{Float64, 2} = cumsum(cumsum(dH, 1), 2)
+	d::Float64 = sum( cum_dH.^2 )
 	
 	return d
 end
