9f666713ef4a3b70e60aaac7ad1c4019d1f1886b
Magnus Röding <magnus.roding@gmail.com>
2017-12-12 08:05:24 +0100

    Adapted read_output to scalar az output from GPU code

--- matlab_tools/read_output.m
+++ matlab_tools/read_output.m
@@ -19,8 +19,17 @@ function [  model, ...
 	number_of_abc_samples = read_key(file_string, 'number_of_abc_samples', 'scalar');
 	m = reshape(read_key(file_string, 'm', 'array'), [number_of_components, number_of_abc_samples]);
 	c = reshape(read_key(file_string, 'c', 'array'), [number_of_components, number_of_abc_samples]);
-	az = reshape(read_key(file_string, 'az', 'array'), [number_of_components, number_of_abc_samples]);
-	dist = read_key(file_string, 'dist', 'array');
+    
+    az = read_key(file_string, 'az', 'array');
+    if numel(az) == number_of_components * number_of_abc_samples
+        az = reshape(az, [number_of_components, number_of_abc_samples]);
+    else
+        az = az(:);
+        az = repmat(az, [3 1]);
+        az = reshape(az, [number_of_components, number_of_abc_samples]);
+    end
+	
+    dist = read_key(file_string, 'dist', 'array');
 	w = read_key(file_string, 'w', 'array');
 	weighting_scheme = read_key(file_string, 'weighting_scheme', 'string');
 	gamma = read_key(file_string, 'gamma', 'scalar');
